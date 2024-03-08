6cd43b6cd0bfe529688f7f19aefbc05a5fb0173a
Marcelo Augusto <157186068+marceloab11@users.noreply.github.com>
2024-03-07 23:43:17 -0300

    Escrita melhorada

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,27 +1,10 @@
 export class Negociacao {
-    private __data:Date;
-    private __quantidade:number;
-    private __valor:number;
 
-    constructor(data:Date, quantidade:number, valor:number) {
-        this.__data = data;
-        this.__quantidade = quantidade;
-        this.__valor = valor;
-    }
-
-    get data():Date {
-        return this.__data
-    }
-
-    get quantidade():number {
-        return this.__quantidade
-    }
-
-    get valor():number {
-        return this.__valor
+    constructor(public readonly data:Date, public readonly quantidade:number, public readonly valor:number) {
+        
     }
 
     get volume():number {
-        return this.__quantidade * this.valor
+        return this.quantidade * this.valor
     }
 }
